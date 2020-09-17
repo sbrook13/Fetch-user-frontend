a1dfa1c87f60aca8efd56bd3ec7225370b3ac642
sbrook13 <sbrook13@gmail.com>
2020-09-17 14:30:27 -0600

    updated fetch options

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ function createUser(event){
         method: 'POST',
         headers: {
             'Accept': 'application/json',
-            'Content-type': 'application/json'
+            'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ username, password })
-    }).then(response => response.json())
+        body: JSON.stringify({ username, password})
+    })
+        .then(response => response.json())
         .then(console.log)
 }
 
@@ -34,11 +35,49 @@ function displayUsers(users){
     users.forEach(user => {
         const card = document.createElement('div')
         const username = document.createElement('p')
+        const editUsernameForm = document.createElement('form')
+        const usernameField = document.createElement('input')
+        const submitButton = document.createElement('input')
+        const deleteButton = document.createElement('button')
 
         card.className = 'card'
         username.textContent = user.username
+        usernameField.name = 'username'
+        usernameField.placeholder = 'Update username'
+        submitButton.type = 'submit'
+        deleteButton.textContent = 'Delete User'
 
-        card.append(username)
+        editUsernameForm.append(usernameField, submitButton)
+        card.append(username, editUsernameForm, deleteButton)
         usersContainer.appendChild(card)
+
+        editUsernameForm.addEventListener('submit', (event) => updateUser(event, user, usernameElement))
+        deleteButton.addEventListener('click', () => deleteUser(user))
     })
+}
+function deleteUser(user){
+    fetch(`${USERS_URL}/${user.id}`,{
+        method: 'DELETE'
+    }).then(response => response.json())
+        .then(console.log)
+}
+
+function updateUser(event, user, usernameElement){
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    const username = formData.get('username')
+
+    usernameElement.textContent = username
+
+    fetch(`${USERS_URL}/${user.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username })
+    }).then(response => response.json())
+        .then(console.log)
+
 }
